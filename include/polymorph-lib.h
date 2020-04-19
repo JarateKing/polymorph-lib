4610f74fcbefa1dc875187b8df9bc70a007eabe2
JarateKing <scmacintosh@upei.ca>
2020-04-19 10:59:30 -0300

    made seed use date as well

--- include/polymorph-lib.h
+++ include/polymorph-lib.h
@@ -2,16 +2,19 @@
 // COMPILE-TIME RNG
 // ================
 
-// we define our seed based off of the __TIME__ string
+// we define our seed based off of the __DATE__ and __TIME__ macros
 // this allows us to have different compile-time seed values
-constexpr unsigned int Seed = (__TIME__[0] - '0') * 1 +
-                              (__TIME__[1] - '0') * 10 +
-                              (__TIME__[3] - '0') * 100 +
-                              (__TIME__[4] - '0') * 1000 +
-                              (__TIME__[6] - '0') * 10000 +
-                              (__TIME__[7] - '0') * 100000;
+constexpr unsigned int Seed = (__DATE__[10] - '0') +
+                              (__DATE__[5] - '0') * 10 +
+                              (__TIME__[0] - '0') * 100 +
+                              (__TIME__[1] - '0') * 1000 +
+                              (__TIME__[3] - '0') * 10000 +
+                              (__TIME__[4] - '0') * 100000 +
+                              (__TIME__[6] - '0') * 1000000 +
+                              (__TIME__[7] - '0') * 10000000;
 
-// we need to use a counter-based rng, to use __COUNTER__ as our state
+// normal prng's are hard to use here, since we can't easily modify our state
+// we need to use a counter-based rng, to use __COUNTER__ as our state instead
 // https://en.wikipedia.org/wiki/Counter-based_random_number_generator_(CBRNG)
 // we use Widynski's Squares method to achieve this: https://arxiv.org/abs/2004.06278
 constexpr unsigned int Widynski_Squares(unsigned int count, unsigned int seed) {
