6c342bc0e9b7c00dbcab36a09ec84dfc8f157e1f
JarateKing <scmacintosh@upei.ca>
2020-04-23 14:29:02 -0300

    added a simple example

---
+++ examples/simple.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/polymorph-lib.h"
+
+int main() {
+	// print out random number "0 <= x < 10000"
+	// this number will only change when you recompile
+	std::cout << poly_random(10000) << '\n';
+}
