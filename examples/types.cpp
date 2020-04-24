ef51b66da2aa62089704e4a03fd0237468204a19
JarateKing <scmacintosh@upei.ca>
2020-04-24 18:33:30 -0300

    added test for different types

---
+++ examples/types.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/polymorph-lib.h"
+
+int main() {
+	std::cout << poly_int() << '\n';
+	std::cout << poly_uint() << '\n';
+	std::cout << poly_ll() << '\n';
+	std::cout << poly_ull() << '\n';
+	std::cout << poly_float() << '\n';
+	std::cout << poly_double() << '\n';
+}
