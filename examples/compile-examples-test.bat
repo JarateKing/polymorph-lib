2f0068dd3b2a68a90f4737eead467b674fccf00f
JarateKing <scmacintosh@upei.ca>
2020-05-24 15:46:06 -0300

    improved formatting of compile test

--- examples/compile-examples-test.bat
+++ examples/compile-examples-test.bat
@@ -1,18 +1,32 @@
+@echo off
+
 rmdir /S /Q output
 mkdir output
+
+echo compiling
 compile-examples.py
+echo compiled
+
+echo.
 echo automatically seeded
 output\random1.exe
 output\random2.exe
 output\random3.exe
+
+echo.
 echo fixed seed
 output\fixed1.exe
 output\fixed2.exe
 output\fixed3.exe
+
+echo.
 echo externally seeded
 output\seeded1.exe
 output\seeded2.exe
 output\seeded3.exe
+
+echo.
 echo types
 output\types.exe
+
 pause
