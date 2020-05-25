c34b7b326ec54f73beefee95ac8522824b6fbb0c
JarateKing <scmacintosh@upei.ca>
2020-05-25 16:28:25 -0300

    made indents consistently spaces

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -2,7 +2,7 @@
 #include "../include/polymorph-lib.h"
 
 int main() {
-	// print out random number "0 <= x < 10000"
-	// this number will only change when you recompile
-	std::cout << poly_random(10000) << '\n';
+    // print out random number "0 <= x < 10000"
+    // this number will only change when you recompile
+    std::cout << poly_random(10000) << '\n';
 }
--- examples/types.cpp
+++ examples/types.cpp
@@ -2,10 +2,10 @@
 #include "../include/polymorph-lib.h"
 
 int main() {
-	std::cout << "int    " << poly_int() << '\n';
-	std::cout << "uint   " << poly_uint() << '\n';
-	std::cout << "ll     " << poly_ll() << '\n';
-	std::cout << "ull    " << poly_ull() << '\n';
-	std::cout << "float  " << poly_float() << '\n';
-	std::cout << "double " << poly_double() << '\n';
+    std::cout << "int    " << poly_int()    << std::endl;
+    std::cout << "uint   " << poly_uint()   << std::endl;
+    std::cout << "ll     " << poly_ll()     << std::endl;
+    std::cout << "ull    " << poly_ull()    << std::endl;
+    std::cout << "float  " << poly_float()  << std::endl;
+    std::cout << "double " << poly_double() << std::endl;
 }
