34c5084b4b9b07eca7ea59bfcd597c9105b84df7
mrky404 <146552991+mrky404@users.noreply.github.com>
2023-10-10 13:27:42 +0200

    Update types.cpp

--- examples/types.cpp
+++ examples/types.cpp
@@ -2,10 +2,13 @@
 #include "../include/polymorph-lib.h"
 
 int main() {
+    // Calling functions from polymorph-lib and print the results
     std::cout << "int    " << poly_int()    << std::endl;
     std::cout << "uint   " << poly_uint()   << std::endl;
     std::cout << "ll     " << poly_ll()     << std::endl;
     std::cout << "ull    " << poly_ull()    << std::endl;
     std::cout << "float  " << poly_float()  << std::endl;
     std::cout << "double " << poly_double() << std::endl;
+
+    return 0; // Indicates successful program execution
 }
